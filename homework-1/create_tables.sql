f1e9d4ffcd9654594e19e58ea2158e0dfaa9f332
adelina010416 <adelinazima@mail.ru>
2023-08-03 19:20:59 +0400

    homework_1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,26 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+	employee_id int PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	title VARCHAR(50),
+	birth_date DATE,
+	notes TEXT
+);
+
+CREATE TABLE customers
+(
+	customer_id VARCHAR(5) PRIMARY KEY,
+	company_name VARCHAR(50),
+	contact_name VARCHAR(50)
+);
+
+CREATE TABLE orders
+(
+	order_id int PRIMARY KEY,
+	customer_id VARCHAR(5) REFERENCES customers(customer_id),
+	employee_id int REFERENCES employees(employee_id),
+	order_date DATE,
+	ship_city VARCHAR(50)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,40 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+conn = psycopg2.connect(host='localhost',
+                        database='north',
+                        user='postgres',
+                        password='1310')
+
+files = ['customers_data.csv', 'employees_data.csv', 'orders_data.csv']
+data = []
+
+for file_name in files:
+    with open(os.path.join('north_data', file_name), newline='', encoding='windows-1251') as file:
+        reader = csv.reader(file)
+        file_data = []
+        for row in reader:
+            row = tuple(row)
+            file_data.append(row)
+
+    data.append(file_data)
+
+table_data = {'customers': data[0],
+              'employees': data[1],
+              'orders': data[2]}
+
+columns = '%s, '
+
+try:
+    with conn:
+        with conn.cursor() as cur:
+            for k, v in table_data.items():
+                columns_counter = len(v[0])
+                for row in v[1:]:
+                    cur.execute(f"INSERT INTO {k} ({', '.join(v[0])}) VALUES ({(columns * columns_counter)[:-2]})", row)
+                print(k, 'converted')
+finally:
+    conn.close()
+
